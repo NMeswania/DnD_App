d77f05a2dfa3dd5ba55708312793209fc19c276b
NMeswania <n.meswania@gmail.com>
2021-05-20 00:48:02 +0100

    Populate setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+###################################################################################################
+# Copyright © 2021 Neal Meswania
+# Lisence: MIT
+###################################################################################################
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+  readme = f.read()
+
+with open('LISENCE') as f:
+  license = f.read()
+
+setup(name='dnd_app',
+      version='0.0.0',
+      entry_points={
+          'console_scripts': ['dnd_app = dnd_app.main:main',],
+      },
+      description='D&D app to manae character sheets',
+      long_description=readme,
+      author='Neal Meswania',
+      url='https://github.com/NMeswania/DnD_App',
+      license=license,
+      packages=find_packages(exclude=('tests', 'docs')))
