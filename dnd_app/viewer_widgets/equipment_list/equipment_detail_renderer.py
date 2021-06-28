ce4afae4fb22cd337ee153efd8287a11268f6826
NMeswania <n.meswania@gmail.com>
2021-06-29 00:43:23 +0100

    Migrate eqipment list to kv

--- dnd_app/viewer_widgets/equipment_list/equipment_detail_renderer.py
+++ dnd_app/viewer_widgets/equipment_list/equipment_detail_renderer.py
@@ -3,17 +3,11 @@
 # Lisence: MIT
 ###################################################################################################
 
-from functools import partial
-
 from kivy.properties import ObjectProperty    #pylint: disable=no-name-in-module
 from kivy.factory import Factory
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 
-from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChildren
+from dnd_app.utilities.text_utils import StrFieldToReadable
 
 ###################################################################################################
 ###################################################################################################
@@ -32,7 +26,6 @@ class EquipmentDetailRenderer(Popup):
     super().__init__()
     self._widget = widget
     self._is_open = False
-    self.size_hint = (0.6, 0.6)
 
 ###################################################################################################
 
@@ -72,6 +65,7 @@ class EquipmentDetailRenderer(Popup):
 
   def _Close(self):
     self._is_open = False
+    self.Clear()
     self.dismiss()
 
 ###################################################################################################
--- dnd_app/viewer_widgets/equipment_list/equipment_list_renderer.py
+++ dnd_app/viewer_widgets/equipment_list/equipment_list_renderer.py
@@ -5,13 +5,12 @@
 
 from functools import partial
 
+from kivy.properties import ObjectProperty    #pylint: disable=no-name-in-module
+from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 
 from dnd_app.core.config import Config
-from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChildren
+from dnd_app.utilities.text_utils import StrFieldToReadable
 
 ###################################################################################################
 ###################################################################################################
@@ -20,13 +19,16 @@ from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChi
 
 class EquipmentListRenderer(BoxLayout):
 
+  ids = {}
+  ids['money'] = ObjectProperty("")
+  ids['equipment'] = ObjectProperty("")
+
   def __init__(self, config: Config, widget):
     super().__init__(orientation="vertical")
     self._dnd_config = config
     self._widget = widget
-    self.add_widget(self._AddTitle())
-    self.add_widget(self._AddContent())
     self._equipment_list = []
+    self._AddMoney()
 
 ###################################################################################################
 
@@ -36,10 +38,9 @@ class EquipmentListRenderer(BoxLayout):
 ###################################################################################################
 
   def Clear(self):
-    self._equipment_layout.clear_widgets()
-    for child in self.walk(restrict=True):
-      if hasattr(child, "id"):
-        child.text = ""
+    self.ids['equipment'].clear_widgets()
+    for child in self.ids['money'].children:
+      child.ids['value'].text = ""
     self._equipment_list = []
 
 ###################################################################################################
@@ -49,12 +50,13 @@ class EquipmentListRenderer(BoxLayout):
     for _, v in data.items():
       if isinstance(v, list) and len(v) > 0:
         for item in v:
-          self._equipment_layout.add_widget(self._AddEquipment(item))
+          self._AddEquipment(item)
       elif isinstance(v, dict):
         for key, value in v.items():
-          for child in self.walk(restrict=True):
-            if hasattr(child, "id") and child.id == key:
-              child.text = str(value)
+          for child in self.ids['money'].children:
+            if key == child.ids['name']:
+              child.ids['value'].text = str(value)
+              break
 
 ###################################################################################################
 
@@ -64,43 +66,22 @@ class EquipmentListRenderer(BoxLayout):
 
 ###################################################################################################
 
-  def _AddTitle(self) -> Label:
-    return Label(text="Equipment", font_size="20sp", size_hint=(1, 0.05))
-
-###################################################################################################
-
-  def _AddContent(self) -> BoxLayout:
-    layout = BoxLayout(orientation="vertical")
-    self._equipment_layout = GridLayout(cols=4, row_default_height=40, row_force_default=True)
-    layout.add_widget(self._AddMoney())
-    layout.add_widget(self._equipment_layout)
-    return layout
-
-###################################################################################################
-
-  def _AddMoney(self) -> BoxLayout:
-    layout = BoxLayout(orientation="vertical")
+  def _AddMoney(self):
     for value in ["platinum", "gold", "electrum", "silver", "copper"]:
-      internal_layout = BoxLayout(orientation="horizontal")
-      internal_layout.add_widget(Label(text=StrFieldToReadable(value), size_hint=(0.3, 1)))
-      money_label = Label(text="", size_hint=(0.7, 1))
-      money_label.id = value
-      internal_layout.add_widget(money_label)
-      layout.add_widget(internal_layout)
-    return layout
+      layout = Factory.EquipmentListRendererMoneyLabel()
+      layout.ids['name'] = value
+      layout.ids['demarkation'].text = StrFieldToReadable(value)
+      self.ids['money'].add_widget(layout)
 
 ###################################################################################################
 
-  def _AddEquipment(self, equipment_name: str) -> Button:
+  def _AddEquipment(self, equipment_name: str):
     self._equipment_list.append(equipment_name)
-    btn = Button(text=StrFieldToReadable(equipment_name),
-                 size_hint=(0.9, 1),
-                 font_size="13sp",
-                 padding=(5, 5))
-    AlignWidgetLabelChildren(btn)
+    btn = Factory.EquipmentListRendererEquipmentButton()
+    btn.text = StrFieldToReadable(equipment_name)
     btn.bind(on_press=partial(self._widget.RequestEquipmentCallback, equipment_name,
-                              len(self._equipment_list)))    # pylint: disable=no-member
-    return btn
+                              len(self._equipment_list) - 1))   # pylint: disable=no-member
+    self.ids['equipment'].add_widget(btn)
 
 ###################################################################################################
 ###################################################################################################
