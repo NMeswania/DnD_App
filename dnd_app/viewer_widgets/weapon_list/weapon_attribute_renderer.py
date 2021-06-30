85dc224f5dbb8ab885827fc32bda0a0d61693ded
NMeswania <n.meswania@gmail.com>
2021-06-30 23:50:00 +0100

    Migrate waepons attribute renderer to kv

--- dnd_app/viewer_widgets/weapon_list/weapon_attribute_renderer.py
+++ dnd_app/viewer_widgets/weapon_list/weapon_attribute_renderer.py
@@ -3,13 +3,8 @@
 # Lisence: MIT
 ###################################################################################################
 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.properties import ObjectProperty    #pylint: disable=no-name-in-module
 from kivy.uix.popup import Popup
-
-from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChildren
 
 ###################################################################################################
 ###################################################################################################
@@ -18,13 +13,14 @@ from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChi
 
 class WeaponAttributeRenderer(Popup):
 
+  ids = {}
+  ids['name'] = ObjectProperty("")
+  ids['attribute_description'] = ObjectProperty("")
+
   def __init__(self, widget):
     super().__init__()
     self._widget = widget
-    self.title = "Weapon Attribute"
-    self.content = self._AddContent()
     self._is_open = False
-    self.size_hint = (0.4, 0.4)
 
 ###################################################################################################
 
@@ -34,9 +30,8 @@ class WeaponAttributeRenderer(Popup):
 ###################################################################################################
 
   def Clear(self):
-    for widget in self.walk(restrict=True):
-      if hasattr(widget, "id") and isinstance(widget, Label):
-        widget.text = ""
+    for v in self.ids.values():
+      v.text = ""
 
 ###################################################################################################
 
@@ -52,47 +47,16 @@ class WeaponAttributeRenderer(Popup):
 
   def _UpdateInternal(self, weapon_data: dict):
     for k, v in weapon_data.items():
-      for child in self.walk(restrict=True):
-        if hasattr(child, "id") and child.id == k:
-          child.text = v
+      for k_, v_ in self.ids.items():
+        if k_ == k:
+          v_.text = v
           break
 
 ###################################################################################################
 
-  def _Close(self, instance):
+  def _Close(self):
     self._is_open = False
     self.dismiss()
-
-###################################################################################################
-
-  def _AddContent(self):
-    layout = BoxLayout(orientation="vertical")
-    layout.add_widget(self._AddWeaponName(0.2))
-    layout.add_widget(self._AddDescription(0.7))
-    layout.add_widget(self._AddButtonBar(0.1))
-    return layout
-
-###################################################################################################
-
-  def _AddWeaponName(self, h: float) -> Button:
-    label = Label(text="", font_size="18sp", size_hint=(1, h))
-    label.id = "name"
-    return label
-
-###################################################################################################
-
-  def _AddDescription(self, h: float) -> Label:
-    label = Label(text="", font_size="12sp", size_hint=(1, h))
-    label.id = "attribute_description"
-    AlignWidgetLabelChildren(label, valign="top")
-    return label
-
-###################################################################################################
-
-  def _AddButtonBar(self, h: float) -> Button:
-    close_button = Button(text="Close", size_hint=(1, h), font_size="15sp", padding=(5, 5))
-    close_button.bind(on_press=self._Close)    # pylint: disable=no-member
-    return close_button
 
 
 ###################################################################################################
