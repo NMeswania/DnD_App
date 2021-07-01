61dad348333a44b1da736efc4c37a5348e1a483c
NMeswania <n.meswania@gmail.com>
2021-07-01 01:29:35 +0100

    Migrate weapon list renderer to kv

--- dnd_app/viewer_widgets/weapon_list/weapon_list_renderer.py
+++ dnd_app/viewer_widgets/weapon_list/weapon_list_renderer.py
@@ -5,13 +5,12 @@
 
 from functools import partial
 
+from kivy.properties import ObjectProperty    #pylint: disable=no-name-in-module
+from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 
 from dnd_app.core.config import Config
-from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChildren
+from dnd_app.utilities.text_utils import StrFieldToReadable
 
 ###################################################################################################
 ###################################################################################################
@@ -20,12 +19,13 @@ from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChi
 
 class WeaponListRenderer(BoxLayout):
 
+  ids = {}
+  ids['weapon_table'] = ObjectProperty("")
+
   def __init__(self, config: Config, widget):
-    super().__init__(orientation="vertical")
+    super().__init__()
     self._dnd_config = config
     self._widget = widget
-    self.add_widget(self._AddTitle())
-    self.add_widget(self._AddContent())
 
 ###################################################################################################
 
@@ -35,7 +35,7 @@ class WeaponListRenderer(BoxLayout):
 ###################################################################################################
 
   def Clear(self):
-    self._weapon_layout.clear_widgets()
+    self.ids['weapon_table'].clear_widgets()
 
 ###################################################################################################
 
@@ -44,51 +44,17 @@ class WeaponListRenderer(BoxLayout):
     for v in data.values():
       for weapon in v:
         if isinstance(weapon, list):
-          self._weapon_layout.add_widget(self._AddWeapon(weapon))
+          self._AddWeapon(weapon)
 
 ###################################################################################################
 
-  def _AddTitle(self) -> Label:
-    return Label(text="Weapons", font_size="20sp", size_hint=(1, 0.05))
-
-###################################################################################################
-
-  def _AddContent(self) -> GridLayout:
-    table_layout = GridLayout(cols=1, row_default_height=40, padding=5)
-    self._weapon_layout = GridLayout(cols=1,
-                                     row_force_default=True,
-                                     row_default_height=40,
-                                     padding=5)
-
-    table_headings_layout = BoxLayout(orientation="horizontal")
-    table_headings_layout.add_widget(Label(text="Weapon name", size_hint=(0.3, 1)))
-    table_headings_layout.add_widget(Label(text="ATK", size_hint=(0.3, 1)))
-    table_headings_layout.add_widget(Label(text="DMG", size_hint=(0.3, 1)))
-    AlignWidgetLabelChildren(table_headings_layout)
-
-    table_layout.add_widget(table_headings_layout)
-    table_layout.add_widget(self._weapon_layout)
-    return table_layout
-
-###################################################################################################
-
-  def _AddWeapon(self, weapon_data: dict) -> BoxLayout:
-    layout = BoxLayout(orientation="horizontal")
-    layout.add_widget(self._AddWeaponButton(weapon_data[0]))
-    layout.add_widget(Label(text=weapon_data[1], size_hint=(0.3, 1)))
-    layout.add_widget(Label(text=weapon_data[2], size_hint=(0.3, 1)))
-    return layout
-
-###################################################################################################
-
-  def _AddWeaponButton(self, weapon_name: str) -> Button:
-    btn = Button(text=StrFieldToReadable(weapon_name),
-                 size_hint=(0.3, 1),
-                 font_size="13sp",
-                 padding=(5, 5))
-    AlignWidgetLabelChildren(btn)
-    btn.bind(on_press=partial(self._widget.RequestCallback, "weapon", weapon_name))    # pylint: disable=no-member
-    return btn
+  def _AddWeapon(self, weapon_data: dict):
+    layout = Factory.WeaponListRendererWeaponRow()
+    layout.ids['name'].text = StrFieldToReadable(weapon_data[0])
+    layout.ids['attack'].text = StrFieldToReadable(weapon_data[1])
+    layout.ids['damage'].text = StrFieldToReadable(weapon_data[2])
+    layout.ids['name'].bind(on_press=partial(self._widget.RequestCallback, "weapon", weapon_data[0]))    # pylint: disable=no-member
+    self.ids['weapon_table'].add_widget(layout)
 
 
 ###################################################################################################
