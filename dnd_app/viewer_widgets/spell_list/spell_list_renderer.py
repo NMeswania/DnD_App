31dd75ee9d1d1c8eae69efbeed317dfc4bb3efa6
NMeswania <n.meswania@gmail.com>
2021-07-02 01:45:48 +0100

    Migrate spell list renderer to kv

--- dnd_app/viewer_widgets/spell_list/spell_list_renderer.py
+++ dnd_app/viewer_widgets/spell_list/spell_list_renderer.py
@@ -5,14 +5,12 @@
 
 from functools import partial
 
+from kivy.properties import ObjectProperty    #pylint: disable=no-name-in-module
+from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.checkbox import CheckBox
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 
 from dnd_app.core.config import Config
-from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChildren
+from dnd_app.utilities.text_utils import StrFieldToReadable
 
 ###################################################################################################
 ###################################################################################################
@@ -21,12 +19,16 @@ from dnd_app.utilities.text_utils import StrFieldToReadable, AlignWidgetLabelChi
 
 class SpellListRenderer(BoxLayout):
 
+  ids = {}
+  ids['ability'] = ObjectProperty("")
+  ids['save'] = ObjectProperty("")
+  ids['attack'] = ObjectProperty("")
+  ids['spell_layout'] = ObjectProperty("")
+
   def __init__(self, config: Config, widget):
     super().__init__(orientation="vertical")
     self._dnd_config = config
     self._widget = widget
-    self.add_widget(self._AddTitle())
-    self.add_widget(self._AddContent())
     self._spells = []
 
 ###################################################################################################
@@ -37,25 +39,17 @@ class SpellListRenderer(BoxLayout):
 ###################################################################################################
 
   def Clear(self):
-    self._spell_layout.clear_widgets()
+    self.ids['spell_layout'].clear_widgets()
+    for v in self.ids.values():
+      if hasattr(v, "text"):
+        v.text = ""
     self._spells = []
 
 ###################################################################################################
 
   def Update(self, data: dict):
     self.Clear()
-    for k, v in data.items():
-      if isinstance(v, list) and len(v) > 0:
-        level_layout = self._AddSpellLevel(k)
-        for spell in v:
-          level_layout.add_widget(self._AddSpell(spell_name=spell))
-        self._spell_layout.add_widget(level_layout)
-      elif isinstance(v, dict):
-        for k_, v_ in v.items():
-          for child in self.walk(restrict=True):
-            if hasattr(child, "id") and child.id == k_:
-              child.text = str(v_)
-              break
+    self._UpdateInternal(data)
 
 ###################################################################################################
 
@@ -65,76 +59,33 @@ class SpellListRenderer(BoxLayout):
 
 ###################################################################################################
 
-  def _AddContent(self):
-    layout = BoxLayout(orientation="vertical")
-    layout.add_widget(self._AddInfo())
-    self._spell_layout = self._AddSpells()
-    layout.add_widget(self._spell_layout)
-    return layout
+  def _UpdateInternal(self, data):
+    for k, v in data.items():
+      if isinstance(v, list) and len(v) > 0:
+        self._AddSpellLevel(k, v)
+      elif isinstance(v, dict):
+        self._UpdateInternal(v)
+      else:
+        for k_, v_ in self.ids.items():
+          if k_ == k:
+            v_.text = str(v)
+            break
 
 ###################################################################################################
 
-  def _AddTitle(self) -> Label:
-    return Label(text="Spells", font_size="20sp", size_hint=(1, 0.05))
+  def _AddSpellLevel(self, level: str, spells: list):
+    layout = Factory.SpellListRendererSpellLevel()
+    layout.ids['spell_level'].text = StrFieldToReadable(level, strip_end_numbers=False)
 
-###################################################################################################
+    for spell in spells:
+      self._spells.append(spell)
+      spell_layout = Factory.SpellListRendererSpell()
+      spell_layout.ids['name'].text = StrFieldToReadable(spell)
+      spell_layout.ids['name'].bind(on_press=partial(self._widget.RequestSpellCallback, spell,
+                                                     len(self._spells) - 1))    # pylint: disable=no-member
+      layout.add_widget(spell_layout)
 
-  def _AddInfo(self) -> BoxLayout:
-    layout = BoxLayout(orientation="horizontal", height=40)
-    for field in ["ability", "save", "attack"]:
-      layout.add_widget(self._AddInfoLabel(field))
-    return layout
-
-###################################################################################################
-
-  def _AddInfoLabel(self, field: str) -> BoxLayout:
-    layout = BoxLayout(orientation="horizontal")
-    layout.add_widget(
-        Label(text=StrFieldToReadable(field), font_size="13sp", italic=True, size_hint=(0.4, 1)))
-    label = Label(text="", font_size="14sp", size_hint=(0.6, 1))
-    label.id = field
-    layout.add_widget(label)
-    return layout
-
-###################################################################################################
-
-  def _AddSpells(self) -> GridLayout:
-    layout = GridLayout(cols=5)
-    return layout
-
-###################################################################################################
-
-  def _AddSpellLevel(self, level: str) -> GridLayout:
-    layout = GridLayout(cols=1, row_force_default=True, row_default_height=40, padding=5)
-    layout.id = level
-    layout.add_widget(
-        Label(text=StrFieldToReadable(level, strip_end_numbers=False), size_hint=(1, 1)))
-    return layout
-
-###################################################################################################
-
-  def _AddSpell(self, spell_name: str) -> BoxLayout:
-    self._spells.append(spell_name)
-    layout = BoxLayout(orientation="horizontal", size=(100, 1))
-    layout.add_widget(self._AddSpellCheckBox())
-    layout.add_widget(self._AddSpellButton(spell_name))
-    return layout
-
-###################################################################################################
-
-  def _AddSpellButton(self, spell_name: str) -> Button:
-    btn = Button(text=StrFieldToReadable(spell_name),
-                 size_hint=(0.9, 1),
-                 font_size="13sp",
-                 padding=(5, 5))
-    AlignWidgetLabelChildren(btn)
-    btn.bind(on_press=partial(self._widget.RequestSpellCallback, spell_name, len(self._spells)))    # pylint: disable=no-member
-    return btn
-
-###################################################################################################
-
-  def _AddSpellCheckBox(self) -> CheckBox:
-    return CheckBox(active=False, size_hint=(0.1, 1))
+    self.ids['spell_layout'].add_widget(layout)
 
 
 ###################################################################################################
